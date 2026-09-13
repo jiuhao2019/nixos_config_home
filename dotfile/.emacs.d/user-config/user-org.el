4cff8401fd94842eb128d9a77506196164a09f17
jiuhao <815514981@qq.com>
2026-09-13 21:31:30 +0800

    2026-09-13 21:31:30

--- dotfile/.emacs.d/user-config/user-org.el
+++ dotfile/.emacs.d/user-config/user-org.el
@@ -108,6 +108,11 @@
 ;; 右边提示有括号，符号不用下划线
 (set-face-attribute 'org-ellipsis nil :underline nil)
 
+(defun my-org-open-at-point-in-tab ()
+  (interactive)
+  (tab-new)
+  (org-open-at-point))
+
 ;; 所有 #+begin_src xxx 和 #+end_src 本身字符都会被隐藏
 (defun my/org-hide-block-delimiters ()
   "Hide #+begin_xxx / #+end_xxx lines in org-mode."
