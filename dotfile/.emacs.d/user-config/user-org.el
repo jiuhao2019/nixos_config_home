4eb03a321f038bf80e79228f5ba9bb7e50392aea
jiuhao <815514981@qq.com>
2026-09-13 23:38:22 +0800

    2026-09-13 23:38:22

--- dotfile/.emacs.d/user-config/user-org.el
+++ dotfile/.emacs.d/user-config/user-org.el
@@ -113,6 +113,7 @@
   (tab-new)
   (apply orig-fun args))
 (advice-add 'org-open-at-point :around #'my-org-open-at-point-in-tab)
+(global-set-key (kbd "C-c o") #'org-open-at-point)
 
 ;; 所有 #+begin_src xxx 和 #+end_src 本身字符都会被隐藏
 (defun my/org-hide-block-delimiters ()
