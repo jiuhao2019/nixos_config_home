d7073c9abee0a2ffd66f7887bffd8b2faa21b065
jiuhao <815514981@qq.com>
2026-09-25 18:04:18 +0800

    2026-09-25 18:04:18

--- .config/wezterm/cursor.lua
+++ .config/wezterm/cursor.lua
@@ -1,10 +1,4 @@
 return {
-	-- colors = {
-	-- 	-- 光标
-	-- 	cursor_fg = "#282828", -- 光标框中文字颜色
-	-- 	cursor_bg = "#ebdbb2", -- 光标块颜色
-	-- 	cursor_border = "#ebdbb2", -- 光标边框颜色
-	-- },
 	-- Acceptable values are SteadyBlock, BlinkingBlock, SteadyUnderline, BlinkingUnderline, SteadyBar, and BlinkingBar.
-	default_cursor_style = "BlinkingUnderline",
+	default_cursor_style = "SteadyBlock",
 }
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -19,5 +19,12 @@ local config = utils.merge(table.unpack(modules))
 config.color_scheme = "Gruvbox Dark (Gogh)"
 config.colors = {
 	background = "#32302f",
+
+	colors = {
+		-- 光标
+		cursor_fg = "#282828", -- 光标框中文字颜色
+		cursor_bg = "#ebdbb2", -- 光标块颜色
+		cursor_border = "#ebdbb2", -- 光标边框颜色
+	},
 }
 return config
