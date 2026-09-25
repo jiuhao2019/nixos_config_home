a0f9e70a2e8ed99659ab8f7eef93a58a0c5cf9c9
jiuhao <815514981@qq.com>
2026-09-25 19:23:34 +0800

    2026-09-25 19:23:34

--- .config/nvim/lua/plugin/colorize.lua
+++ .config/nvim/lua/plugin/colorize.lua
@@ -1,11 +1,2 @@
 require("colorizer").setup({
-	user_default_options = {
-		RGB = true,
-		RRGGBB = true,
-		RRGGBBAA = true,
-		names = false,
-
-		mode = "virtualtext",
-		virtualtext = "■",
-	},
 })
